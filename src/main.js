2e28fa13d22ae3daebe9c20383084543ecc8e826
Raul Shpilev <raulshpilev@gmail.com>
2017-09-19 19:36:04 +0200

    inital values for testing

--- src/main.js
+++ src/main.js
@@ -20,9 +20,52 @@ new Vue({
   data: {
     /* VueStash Store */
     store: {
-      teachers: [],
-      students: [],
-      lessons: [],
+      teachers: [
+        {
+          name: 'Teacher 1',
+          lessons: [1, 2]
+        },
+        {
+          name: 'Teacher 2',
+          lessons: [2, 3]
+        },
+        {
+          name: 'Teacher 3',
+          lessons: [3, 4]
+        }
+      ],
+      students: [
+        {
+          name: 'Student 1'
+        },
+        {
+          name: 'Student 2'
+        },
+        {
+          name: 'Student 3'
+        },
+        {
+          name: 'Student 4'
+        }
+      ],
+      lessons: [
+        {
+          name: 'lesson 1',
+          students: [1, 2]
+        },
+        {
+          name: 'lesson 2',
+          students: [3, 4]
+        },
+        {
+          name: 'lesson 3',
+          students: [2, 3]
+        },
+        {
+          name: 'lesson 4',
+          students: [1, 2]
+        }
+      ],
       grades: []
     }
   },
