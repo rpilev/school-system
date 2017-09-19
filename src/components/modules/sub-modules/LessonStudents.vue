a6bfb0a363afbbed11c98f7ee2c711938f4b3cde
Raul Shpilev <raulshpilev@gmail.com>
2017-09-19 14:19:13 +0200

    Include sub-modules dir

---
+++ src/components/modules/sub-modules/LessonStudents.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <router-link to="/lessons" class="button button is-success"  active-class="active">
+      Back
+    </router-link>
+    <h2>Lesson <i>{{ lesson_name }}</i></h2>
+    <aside class="menu">
+      <p class="menu-label">
+        Lessons
+      </p>
+      <ul class="menu-list">
+        <!-- List all available lessons -->
+        <li v-for="(student, index) in all_students">
+          <label class="checkbox">
+            <input @click='logit'
+              v-model='$store.lessons[lesson_id].students'
+              :value="index"
+              type="checkbox"
+            >
+            {{ student.name }}
+          </label>
+        </li>
+      </ul>
+    </aside>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        lesson_id: this.$route.params.id,
+        lesson_name: this.$store.lessons[this.$route.params.id].name,
+        
+        all_students: this.$store.students
+      }
+    },
+    methods: {
+      logit(){
+        console.log(this.$store.lessons[this.lesson_id].students);
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  label {
+    font-size: 22px;
+    color: rgb(92, 92, 92);
+  }
+</style>
---
+++ src/components/modules/sub-modules/TeacherLessons.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <router-link to="/teachers" class="button button is-success"  active-class="active">
+      Back
+    </router-link>
+    <h2>Teacher <i>{{ teacher_name }}</i></h2>
+    <aside class="menu">
+      <p class="menu-label">
+        Lessons
+      </p>
+      <ul class="menu-list">
+        <!-- List all available lessons -->
+        <li v-for="(lesson, index) in all_lessons">
+          <label class="checkbox">
+            <!-- v-model the selection directly to the store attr -->
+            <input 
+              v-model='$store.teachers[teacher_id].lessons'
+              :value="index"
+              type="checkbox"
+            >
+            {{ lesson.name }}
+          </label>
+        </li>
+      </ul>
+    </aside>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        teacher_id: this.$route.params.id,
+        teacher_name: this.$store.teachers[this.$route.params.id].name,
+        
+        all_lessons: this.$store.lessons
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  label {
+    font-size: 22px;
+    color: rgb(92, 92, 92);
+  }
+</style>
