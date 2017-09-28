270a7c5b95d0ac5c85d971b0f1836a28f28f9711
Raul Shpilev <raulshpilev@gmail.com>
2017-09-28 13:14:21 +0200

    Refactor components

---
+++ src/components/forms/add_unit.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <a 
+      v-if='!showAddForm'
+      class="button is-info add-button"
+      @click='toggleAddUnit'
+    >Add Teacher</a>
+
+    <!-- Teacher add form -->
+    <div v-else class="field">
+      <h2>Add {{ type | capitalize }}</h2>
+      <label class="label">Name</label>
+      <div class="control">
+        <input 
+          :class="{ input : true, 'is-danger' : !form_valid }"
+          v-model='unit_name'
+          @input='form_valid = true'
+          type="text"
+          placeholder="Teacher Name"
+        >
+      </div>
+      <p v-show='!form_valid' class="help is-danger">Please enter a name</p>
+      <br>
+      <div class="control">
+        <button 
+          @click='addUnit()'
+          class="button is-primary"
+        >Submit</button>
+        <button 
+          @click='showAddForm = false;
+                  unit_name ="";
+                  form_valid = true;'
+          class="button is-danger"
+        >Cancel</button>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    props: [
+      'type',
+      'association'
+    ],
+    data() {
+      return {
+        showAddForm: false,
+        unit_name: '',
+        form_valid: true
+      }
+    }
+  }
+</script>
---
+++ src/components/tables/unit_table.vue
@@ -0,0 +1,95 @@
+<template>
+  <div>
+    <table v-show='$store[type + "s"].length != 0' class="table">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <!-- only show if this table requires associations -->
+          <th v-if='table_association'>{{ table_association | capitalize }}</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tfoot>
+        <tr>
+          <th>Name</th>
+          <th v-if='table_association'>{{ table_association | capitalize }}</th>
+          <th>Actions</th>
+        </tr>
+      </tfoot>
+      <transition-group name='slide' tag="tbody">
+        <!-- check if index for deleted items -->
+        <tr
+          :key="index"
+          v-for='(unit, index) in $store[type + "s"]'
+          v-if='unit != "deleted"'>
+          
+          
+          <td class="table-restricted-size">
+            <transition name='slide' mode='out-in'>
+              <!-- If the current index is being edited show an input field, otherwise just the name -->
+              <div key='1' v-if='editing == index'>
+                <input  
+                 :class="{ 'edit-input input' : true, 'is-danger' : !form_valid }"
+                 type="text"
+                 name=""
+                 v-model="unit_name"
+                 @input="form_valid = true"
+                >
+                <button @click="submitEdit(index)" class="button is-info custom-edit-button">Ok</button>
+                <button @click="editing=-1;unit_name=''" class="button is-danger custom-edit-button">Cancel</button>
+                <span v-show='!form_valid' class="help is-danger">Please enter a name</span>
+              </div>
+                
+              <div key='2' v-else>
+                {{ unit.name }}
+              </div>
+            </transition>
+          </td>
+          <td  
+            v-if='table_association'
+            style="text-align: center;
+            color: #6ba3ff;
+            cursor: pointer;"
+          >
+            <router-link :to="'/'+ type +'/' + index + '/' + table_association + '/'">
+              {{  unit[table_association].length }}
+              <!-- plural grammar -->
+              <span v-if='unit[table_association].length == 1'>{{ table_association | capitalize | removeLastCharacter }}</span>
+              <span v-else>{{ table_association | capitalize }}</span>
+            </router-link>
+          </td>
+          <td 
+            style="cursor: pointer;
+            text-align:center;
+            font-weight: bolder;"
+          >
+            <!-- lesson name edit and delete buttons -->
+            <span @click = 'editUnit(index, unit.name)'>
+              <icon style='color:green;'  name='pencil'></icon>
+            </span>
+            <span @click = 'removeUnit(index)'>
+              <icon style='color:red;'  name='trash'></icon>
+            </span>
+          </td>
+        </tr>
+      </transition-group>
+    </table>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: [
+      'type',
+      'association',
+      'table_association'
+    ],
+    data() {
+      return {
+        unit_name: '',
+        form_valid: true,
+        editing: -1
+      }
+    }
+  }
+</script>
